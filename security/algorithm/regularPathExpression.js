2208c8d4359448566a72f51ae41fa1ad48c11479
Valentijn Spruyt <Valentijnspruyt@gmail.com>
2015-12-26 15:40:28 +0100

    Added rege rregex fluent API (TODO: Construction Algotithm)

--- security/algorithm/regularPathExpression.js
+++ security/algorithm/regularPathExpression.js
@@ -1,27 +1,139 @@
 function RegularPathExpression(){ 
 	this.expression = [];
+
+	//Index x represents edge with label 'x' in the NFA 
+	this._map = [];
+	//Check braces
+	this.depth = 0;
 }
+
+/**
+ * ----------------
+ * THINGS TO DETECT
+ * ----------------
+ */
 
 //Assignment
 RegularPathExpression.prototype.assign = function(obj){
-
-
+	this._map.push(new RegexPart('assign', obj));
 	//Fluent API
 	return this;
 }
 
 //Function calls
 RegularPathExpression.prototype.fCall = function(obj){
+	this._map.push(new RegexPart('fCall', obj));
+	//Fluent API
+	return this;
+}
+
+/**
+ * ----------
+ * DELIMITERS
+ * ----------
+ */
+
+//Left brace
+RegularPathExpression.prototype.lBrace = function(obj){
+	this.depth++;
+	this._map.push(new RegexPart('lBrace', obj, '('));
+	//Fluent API
+	return this;
+}
+
+//Right brace
+RegularPathExpression.prototype.rBrace = function(obj){
+	if(this.depth === 0) throw 'Can\'t close a non-existing brace. Check brace order';
+	
+	this.depth--;
+	this._map.push(new RegexPart('rBrace', obj, ')'));
+	//Fluent API
+	return this;
+}
+
+//or
+RegularPathExpression.prototype.or = function(obj){
+
+	this._map.push(new RegexPart('or', obj, '|'));
+	//Fluent API
+	return this;
+}
 
 
+
+//Wildcard
+RegularPathExpression.prototype.wildcard = function(obj){
+
+	this._map.push(new RegexPart('wildcard', obj, '_'));
 	//Fluent API
 	return this;
 }
 
 //Wildcard
-RegularPathExpression.prototype.__ = function(obj){
+RegularPathExpression.prototype.not = function(obj){
 
-
+	this._map.push(new RegexPart('not', obj, '¬'));
 	//Fluent API
 	return this;
 }
+
+//Star
+RegularPathExpression.prototype.star = function(obj){
+
+	this._map.push(new RegexPart('star', obj, '*'));
+	//Fluent API
+	return this;
+}
+
+//Star
+RegularPathExpression.prototype.plus = function(obj){
+
+	this._map.push(new RegexPart('plus', obj, '+'));
+	//Fluent API
+	return this;
+}
+
+/*
+ * ----
+ * MISC
+ * ----
+ */
+
+RegularPathExpression.prototype.toString = function(obj){
+	return this._map.map(function(x){ return x.toString(); }).join('');
+}
+
+RegularPathExpression.prototype.toPrettyString = function(obj){
+	return this._map.map(function(x){ return x.toString() + '\n'; }).join('.');
+}
+
+
+/**
+ * -------------
+ * BUILD NFA/DFA
+ * -------------
+ */
+RegularPathExpression.prototype.toNFA = function(){
+	if(this.depth !== 0) throw 'Not all braces are closed!'; 
+	//TODO: CALL CONVERSION ALGORITHM
+}
+
+
+/**
+ * -----------------------
+ * TESTSTRUCTURE FOR REGEX
+ * -----------------------
+ */
+
+ function RegexPart(name, obj, symbol){
+ 	this.name = name;
+ 	this.symbol = symbol;
+ 	this.obj = obj;
+ }
+
+RegexPart.prototype.toString = function(){
+	if (this.symbol) return this.symbol;
+	var str = this.obj ? JSON.stringify(this.obj) : '';
+	return this.name + '(' + str +')';
+}
+
